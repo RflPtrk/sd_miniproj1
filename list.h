d7c66618571f4e994ffdc9e45cf9d92d7d3b9359
Piotr Rafałko <164638790+RflPtrk@users.noreply.github.com>
2024-03-30 23:14:17 +0100

    Update list.h

--- list.h
+++ list.h
@@ -15,9 +15,13 @@ public:
 	virtual void delete_end() = 0;
 	virtual void delete_ind(int index) = 0;
 
+	virtual void load_file(const std::string& filename) = 0;
+	virtual void save_file(const std::string& filename) = 0;
+	virtual void show() = 0;
+	virtual void clear() = 0;
 	virtual bool isEmpty() = 0;
 	virtual void print() = 0;
-	virtual int return_element(int index) = 0;
+	virtual int find_by_index(int index) = 0;
 	virtual bool find(int element) = 0;
 };
 #endif // !SD1_LIST_H
